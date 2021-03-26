91f26fd10d50a6f289723c4c6058fed61103bd79
Charles Ming <charlesming@berkeley.edu>
2021-03-26 17:42:17 -0400

    update favicon and .env

--- server/controllers/media.js
+++ server/controllers/media.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const getSongs = async (req, res) => {
     try {
-        const response = await axios.get(`https://api-v2.soundcloud.com/search/tracks?client_id=SHBP59ZbnkOWhy3perfU0I83tRB8UuJk&offset=0&linked_partitioning=1&app_locale=en&limit=20&q=${req.query.query}`)
+        const response = await axios.get(`https://api-v2.soundcloud.com/search/tracks?client_id=${process.env.SC_CLIENT_ID}&offset=0&linked_partitioning=1&app_locale=en&limit=20&q=${req.query.query}`)
         res.status(200).json(response.data);
     } catch (error) {
         res.status(404).json({ message: error.message });
