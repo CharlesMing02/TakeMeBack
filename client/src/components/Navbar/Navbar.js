7280db583e21e00b218e1302f1fa46155f57470d
Charles Ming <charlesming@berkeley.edu>
2021-03-27 13:49:21 -0400

    add confirmation/breaking streak to view all; fix bug where daily entry persists

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import { Link, useHistory, useLocation } from 'react-router-dom';
 import { AppBar, Button, Toolbar, Typography, IconButton, Drawer, MenuItem } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import { useDispatch, useSelector } from 'react-redux';
-import { refreshUser } from '../../actions/auth';
+import { refreshUser, updateUser } from '../../actions/auth';
 import decode from 'jwt-decode';
 
 import AvatarMenu from '../AvatarMenu/AvatarMenu';
@@ -46,6 +46,19 @@ const Navbar = () => {
         }
     },[location]);
 
+    const confirmViewAll = () => {
+        console.log(user.result.guessed);
+        if (user.result.guessed) {
+            history.push('/view_all');
+        } else {
+            let answer = window.confirm('Are you sure? Viewing all before guessing will break your streak.');
+            if (answer) {
+                dispatch(updateUser(user.result._id, { streak: 0 }));
+                history.push('/view_all')
+            }
+        }
+    }
+
     const displayMobile = () => {
         const handleDrawerOpen = () => setDrawerOpen(true);
         const handleDrawerClose = () => setDrawerOpen(false);
@@ -56,21 +69,28 @@ const Navbar = () => {
                                 onClick: handleDrawerOpen}}>
                     <MenuIcon/>
                 </IconButton>
-                <Drawer {...{anchor: "left", open: drawerOpen, onClose: handleDrawerClose, className: classes.drawer}}>
-                    <Link to='/view_all' className={classes.sidePanel}>
-                        <MenuItem>View All</MenuItem>
-                    </Link>
-                    <Link to='/leaderboard' className={classes.sidePanel}>
-                        <MenuItem>Leaderboard</MenuItem>
-                    </Link>
-                    <Link to='/' color="inherit" className={classes.sidePanel}>
-                        <MenuItem>Settings</MenuItem>
-                    </Link>
-                </Drawer>
+                {user? (
+                    <Drawer {...{anchor: "left", open: drawerOpen, onClose: handleDrawerClose, className: classes.drawer}}>
+                        <Link onClick={confirmViewAll} className={classes.sidePanel}>
+                            <MenuItem>View All</MenuItem>
+                        </Link>
+                        <Link to='/leaderboard' className={classes.sidePanel}>
+                            <MenuItem>Leaderboard</MenuItem>
+                        </Link>
+                        <Link to='/' color="inherit" className={classes.sidePanel}>
+                            <MenuItem>Settings</MenuItem>
+                        </Link>
+                    </Drawer>
+                ) : null}
                 <Typography component={Link} to='/' variant="h5" className={classes.title}>TakeMeBack</Typography>
-                <div style={{marginLeft: 'auto'}}>
-                    <AvatarMenu user={user} logout={logout}/>
-                </div>
+                {user ? (
+                    <div style={{marginLeft: 'auto'}}>
+                        <AvatarMenu user={user} logout={logout}/>
+                    </div>
+                ) : (
+                    <Button component={Link} to="/auth" variant="contained" color="primary" style={{marginLeft: 'auto'}}>Sign In</Button>
+                )}
+                    
             </Toolbar>
         )
     }
@@ -84,7 +104,7 @@ const Navbar = () => {
                         <Toolbar className={classes.toolbar}>
                             {user ? (
                                 <div className={classes.userOptions}>
-                                    <Typography component={Link} to='/view_all' variant="subtitle1" className={classes.title}>View All</Typography>
+                                    <Typography component={Link} variant="subtitle1" className={classes.title}  onClick={confirmViewAll}>View All</Typography>
                                     <Typography component={Link} to='/leaderboard' variant="subtitle1" className={classes.title}>Leaderboard</Typography>
                                     <Typography component={Link} to='/' variant="subtitle1" className={classes.title}>Settings</Typography>
                                     <AvatarMenu user={user} logout={logout}/>
