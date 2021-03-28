9f7578a62c9fab853876614fa629b2bb5418882e
Charles Ming <charlesming@berkeley.edu>
2021-03-27 23:53:42 -0400

    style leaderboard

--- client/src/components/Leaderboard/Leaderboard.js
+++ client/src/components/Leaderboard/Leaderboard.js
@@ -1,14 +1,49 @@
-import React, { useEffect, useState } from 'react'
-import { Container, Grow, Grid, CircularProgress } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { Container, Grow, CircularProgress, Typography, Tabs, Tab, Box, AppBar, List, ListItem, ListItemText, ListItemAvatar, Avatar } from '@material-ui/core';
 import { useSelector, useDispatch } from 'react-redux';
 import { getUsers } from '../../actions/auth';
 
 import useStyles from './styles';
 
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`full-width-tabpanel-${index}`}
+        aria-labelledby={`full-width-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box p={0}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+};
+  
+function a11yProps(index) {
+    return {
+    id: `full-width-tab-${index}`,
+    'aria-controls': `full-width-tabpanel-${index}`,
+    };
+}
+
 const Leaderboard = () => {
     const classes = useStyles();
     const users = useSelector((state) => state.auth.users);
-    //const [sort, setSort] = useState('points') //sort by points or streak
+    const user = useSelector((state) => state.auth.authData.result);
+    const [value, setValue] = useState(0); //sort by points or streak
     const dispatch = useDispatch();
     const byStreak = users.sort((a, b) => {
         return b.streak - a.streak
@@ -21,19 +56,64 @@ const Leaderboard = () => {
         dispatch(getUsers());
     }, []); //empty array means this only occurs on mount (otherwise would have infinite loop)
 
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    function renderStreak(u) {
+        return (
+            <ListItem key={u._id} selected={user._id===u._id}   className={(byStreak.indexOf(u) === 4 || u === byStreak[byStreak.length - 1]) ? classes.divider : null} >
+                <ListItemAvatar>
+                    <Avatar className={classes.avatar}>{1 + byStreak.findIndex((user) => u._id===user._id)}</Avatar>
+                </ListItemAvatar>
+                <ListItemText primary={u.name} secondary={`Current streak: ${u.streak}`}/>
+            </ListItem>
+        )
+    }
+
+    function renderPoints(u) {
+        return (
+            <ListItem key={u._id} selected={user._id===u._id}   className={(byPoints.indexOf(u) === 4 || u === byPoints[byPoints.length - 1]) ? classes.divider : null} >
+                <ListItemAvatar>
+                    <Avatar className={classes.avatar}>{1 + byPoints.findIndex((user) => u._id===user._id)}</Avatar>
+                </ListItemAvatar>
+                <ListItemText primary={u.name} secondary={`Points: ${u.points}`}/>
+            </ListItem>
+        )
+    }
+
     return (
         !users.length ? <CircularProgress/> : (
             <Grow in>
                 <Container className={classes.main} maxWidth={false}>
-                    <h1>Leaderboard (need to style)</h1>
-                    <h2>By Streak</h2>
-                    {byStreak.map((user) => (
-                        <p>{user.name}: {user.streak}</p>
-                    ))}
-                    <h2>By Points</h2>
-                    {byPoints.map((user) => (
-                        <p>{user.name}: {user.points}</p>
-                    ))}
+                    <Typography variant="h5" className={classes.title}>Leaderboard</Typography>
+                    <div className={classes.board}>
+                        <AppBar position="static" color="default">
+                            <Tabs
+                                value={value}
+                                onChange={handleChange}
+                                indicatorColor="primary"
+                                textColor="primary"
+                                variant="fullWidth"
+                                aria-label="full width tabs example"
+                            >
+                                <Tab label="Streak" {...a11yProps(0)} />
+                                <Tab label="Points" {...a11yProps(1)} />
+                            </Tabs>
+                        </AppBar>
+                        <TabPanel value={value} index={0} className={classes.panel}>
+                            <List disablePadding>
+                                {byStreak.slice(0,5).map((user) => renderStreak(user))}
+                                {renderStreak(user)}
+                            </List>
+                        </TabPanel>
+                        <TabPanel value={value} index={1} className={classes.panel}>
+                            <List disablePadding>
+                                {byPoints.slice(0,5).map((user) => renderPoints(user))}
+                                {renderPoints(user)}
+                            </List>
+                        </TabPanel>
+                    </div>
                 </Container>
             </Grow>
         )
--- client/src/components/Leaderboard/styles.js
+++ client/src/components/Leaderboard/styles.js
@@ -1,11 +1,32 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export default makeStyles(() => ({
+export default makeStyles((theme) => ({
     main: {
-        marginTop: 10,
+        marginTop: 0,
         padding: 0,
         flexGrow: 1,
         display: 'flex',
         flexDirection: 'column',
+        alignItems: 'center',
+        backgroundColor: theme.palette.background.paper,
+    },
+    title: {
+        color: theme.palette.primary.dark,
+        paddingTop: theme.spacing(2)
+    },
+    board: {
+        width: '100%',
+        maxWidth: 360,
+        paddingTop: theme.spacing(2),
+    },
+    panel: {
+        border: `1px solid ${theme.palette.divider}`
+    },
+    avatar: {
+        color: theme.palette.getContrastText(theme.palette.primary.main),
+        backgroundColor: theme.palette.primary.main
+    },
+    divider: {
+        borderBottom: `2px solid ${theme.palette.primary.dark}`
     }
 }));
