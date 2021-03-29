977d2781dc90ebc3882d3eb211903f1c85e701f2
Charles Ming <charlesming@berkeley.edu>
2021-03-29 12:25:51 -0400

    forgot to switch uri

--- server/scheduled_job.js
+++ server/scheduled_job.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const dailyUpdate = async () => {
     try {
-        const res = await axios.post('http://localhost:5000/user', {}); //https://take-me-back.herokuapp.com/
+        const res = await axios.post('https://take-me-back.herokuapp.com/', {}); //http://localhost:5000/user
         console.log(res)
     } catch (error) {
         console.log(error)
